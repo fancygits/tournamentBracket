e92fe3341b5574e207b2da9acabb56d4d883330f
James Luke Johnson <jjohn144@my.westga.edu>
2018-07-03 17:24:16 -0500

    Tested routing to Home, Registration, and Tournament pages

--- tests/e2e/routingSpec.js
+++ tests/e2e/routingSpec.js
@@ -4,17 +4,49 @@ function WelcomePage() {
 	};
 
 	this.getWelcomeH2 = function() {
-		return element(by.id('welcome'));
+		return element(by.id('welcome')).getText();
 	};
+
+	this.getHomeLink = function() {
+		return element(by.id('mainLink'));
+	}
+
+	this.getRegistrationLink = function() {
+		return element(by.id('registrationLink'));
+	}
+
+	this.getTournamentLink = function() {
+		return element(by.id('tournamentLink'));
+	}
+
+	this.countRegistrationLinks = function() {
+		return element.all(by.partialLinkText('egistration')).count();
+	}
 }
 
 describe('Routing Test', function () {
 
-	it('should navigate through the app', function() {
+	it('should navigate to the Welcome Page', function() {
 		var welcomePage = new WelcomePage();
 		welcomePage.open();
-		expect(welcomePage.getWelcomeH2().getText()).toEqual("Welcome");
+		expect(welcomePage.getWelcomeH2()).toEqual("Welcome");
+		expect(welcomePage.getHomeLink().isDisplayed()).toBe(true);
+		expect(welcomePage.getRegistrationLink().isDisplayed()).toBe(true);
+		expect(welcomePage.getTournamentLink().isDisplayed()).toBe(true);
+		expect(welcomePage.countRegistrationLinks()).toBe(2);
+	});
 
+	it('should navigate to the Registration Page', function() {
+		var welcomePage = new WelcomePage();
+		welcomePage.open();
+		welcomePage.getRegistrationLink().click();
+		expect(element(by.id('registrationHeading')).getText()).toEqual("Register Players");
+	});
 
+	it('should navigate to the Tournament Page', function() {
+		var welcomePage = new WelcomePage();
+		welcomePage.open();
+		welcomePage.getTournamentLink().click();
+		expect(element(by.id('tournamentHeading')).getText()).toEqual("Tournament");
 	});
 });
