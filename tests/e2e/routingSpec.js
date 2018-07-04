a621d72a43ef62b16e9ff1163d953d6ad827b339
James Luke Johnson <jjohn144@my.westga.edu>
2018-07-04 09:54:52 -0500

    Refactored routingSpec to use module.exports

--- tests/e2e/routingSpec.js
+++ tests/e2e/routingSpec.js
@@ -1,69 +1,41 @@
-function WelcomePage() {
-	this.open = function() {
-		browser.get('/main.html');
-	};
-
-	this.getWelcomeH2 = function() {
-		return element(by.id('welcome')).getText();
-	};
-
-	this.getHomeLink = function() {
-		return element(by.id('mainLink'));
-	}
-
-	this.getRegistrationLink = function() {
-		return element(by.id('registrationLink'));
-	}
-
-	this.getTournamentLink = function() {
-		return element(by.id('tournamentLink'));
-	}
-
-	this.countRegistrationLinks = function() {
-		return element.all(by.partialLinkText('egistration')).count();
-	}
-}
+var app = require('./bracketsE2EMethods.js');
 
 describe('Routing Test', function () {
 
 	it('should navigate to the Welcome Page', function() {
-		var welcomePage = new WelcomePage();
-		welcomePage.open();
-		expect(welcomePage.getWelcomeH2()).toEqual("Welcome");
-		expect(welcomePage.getHomeLink().isDisplayed()).toBe(true);
-		expect(welcomePage.getRegistrationLink().isDisplayed()).toBe(true);
-		expect(welcomePage.getTournamentLink().isDisplayed()).toBe(true);
-		expect(welcomePage.countRegistrationLinks()).toBe(2);
+		app.open();
+		expect(element(by.id('welcome')).getText()).toEqual("Welcome");
+		expect(element(by.id('mainLink')).isDisplayed()).toBe(true);
+		expect(element(by.id('registrationLink')).isDisplayed()).toBe(true);
+		expect(element(by.id('tournamentLink')).isDisplayed()).toBe(true);
+		expect(element.all(by.partialLinkText('egistration')).count()).toBe(2);
 	});
 
 	it('should navigate to the Registration Page', function() {
-		var welcomePage = new WelcomePage();
-		welcomePage.open();
-		welcomePage.getRegistrationLink().click();
+		app.open();
+		app.goToRegister();
 		expect(element(by.id('registrationHeading')).getText()).toEqual("Register Players");
 	});
 
 	it('should navigate to the Tournament Page', function() {
-		var welcomePage = new WelcomePage();
-		welcomePage.open();
-		welcomePage.getTournamentLink().click();
+		app.open();
+		app.goToTournament();
 		expect(element(by.id('tournamentHeading')).getText()).toEqual("Tournament");
 	});
 
 	it('should navigate to the every Page', function() {
-		var welcomePage = new WelcomePage();
-		welcomePage.open();
-		welcomePage.getHomeLink().click();
+		app.open();
+		app.goToHome();
 		expect(element(by.tagName('p')).getText()).toMatch(/Get started/);
-		welcomePage.getRegistrationLink().click();
+		app.goToRegister();
 		expect(element(by.id('registrationHeading')).getText()).toEqual("Register Players");
-		welcomePage.getTournamentLink().click();
+		app.goToTournament();
 		expect(element(by.id('tournamentHeading')).getText()).toEqual("Tournament");
-		welcomePage.getRegistrationLink().click();
+		app.goToRegister();
 		expect(element(by.id('registrationHeading')).getText()).toEqual("Register Players");
-		welcomePage.getHomeLink().click();
-		expect(welcomePage.getWelcomeH2()).toEqual("Welcome");
-		element(by.linkText('registration')).click();
+		app.goToHome();
+		expect(element(by.id('welcome')).getText()).toEqual("Welcome");
+		app.goToRegister();
 		expect(element(by.id('registrationHeading')).getText()).toEqual("Register Players");
 	});
 });
